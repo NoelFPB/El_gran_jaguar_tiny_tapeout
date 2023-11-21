90c0bf5828028ad24832024dff1888c25606b16f
Noel Prado <111771403+NoelFPB@users.noreply.github.com>
2023-11-21 17:31:06 +0900

    Update main_circuit.v

--- src/main_circuit.v
+++ src/main_circuit.v
@@ -1,3 +1,5 @@
+
+
 `default_nettype none
 
 module tt_um_chip_SP_Soy_de_Zacapa(
@@ -11,602 +13,235 @@ module tt_um_chip_SP_Soy_de_Zacapa(
     input  wire       rst_n     // rst_n_n - low to rst_n
 );
 
+wire n_reset;
+wire [3:0] out;
+wire [3:0] address;
+
+// To use the bidirectional pins as input
+assign uio_oe = 8'h0;
+
+// Inputs
+assign n_reset = uio_in[0];
+assign in = uio_in[4:1];
+assign instr = ui_in;
+
+// Outputs
+assign uo_out[3:0] = address;
+assign uo_out[7:4] = out;
+
+//--------------------------------
+// module cpu(
+//   input clk,
+//   input n_reset,
+//   output [3:0] address,
+//   input [7:0] instr,
+//   input [3:0] in,
+//   output [3:0] out);
+
+  //------------------------
+  // （デバッグ用）モニタの設定
+  //------------------------
+  //initial begin
+    // $monitor("%t: pc = %b, a = %b, b = %b, instr = %b, op = %b, im = %b, select_a = %b, select_b = %b, load0 = %b, load1 = %b, load2 = %b", $time, pc_reg, a_reg, b_reg, instr, op, im, select_a, select_b, load0, load1, load2);
+  //end
+
+  //------------------------
+  // ワイヤ・レジスタの宣言
+  //------------------------
+  wire [3:0] op;
+  wire [3:0] im;
+  wire select_a, select_b;
+  wire load0, load1, load2, load3;
+  wire [3:0] selector_out;
+  wire [3:0] alu_out;
+  wire c; // Carry out
+
+  reg [3:0] pc_reg; // Program counter
+  reg [3:0] a_reg, b_reg; // A, B register
+  reg [3:0] out_reg; // Out port register
+  reg co_reg; // Carry out register
+
+  //------------------------
+  // 外部モジュールとの接続
+  //------------------------
+
+  // データセレクタ
+  data_selector ds (a_reg, b_reg, in, 4'b0000, select_a, select_b, selector_out);
+
+  //------------------------
+  // レジスタ
+  //------------------------
+  always @(posedge clk or negedge n_reset) begin
+    if (!n_reset) begin
+      a_reg <= 0;
+      b_reg <= 0;
+    end
+    else begin
+      a_reg <= load0 ? alu_out : a_reg;
+      b_reg <= load1 ? alu_out : b_reg;
+    end
+  end
+
+  //------------------------
+  // 出力ポート
+  //------------------------
+  assign out = out_reg;
+
+  always @(posedge clk or negedge n_reset) begin
+    if (!n_reset) out_reg <= 0;
+    else out_reg <= load2 ? alu_out : out_reg;
+  end
+
+  //------------------------
+  // プログラムカウンタ
+  //------------------------
+  assign address = pc_reg;
+
+  always @(posedge clk or negedge n_reset) begin
+    if (!n_reset) pc_reg <= 0;
+    else pc_reg <= load3 ? im : pc_reg + 1;
+  end
+
+  //------------------------
+  // ALU と carry レジスタ
+  //------------------------
+  assign {c, alu_out} = selector_out + im;
+
+  always @(posedge clk or negedge n_reset) begin
+    if (!n_reset) co_reg <= 0;
+    else co_reg <= c;
+  end
+
+  //------------------------
+  // 命令のデコード
+  //------------------------
+
+  // 命令を「オペレーションコード」と「イミディエイトデータ」へ分割
+  assign op = instr[7:4]; // オペレーションコード
+  assign im = instr[3:0]; // イミディエイトデータ
+
+  // データセレクタ制御フラグ
+  assign select_a = op[0] | op[3];
+  assign select_b = op[1];
+
+  // ロードレジスタ制御フラグ
+  assign load0 = !(op[2] | op[3]); // Aレジスタへロード
+  assign load1 = !(!op[2] | op[3]); // Bレジスタへロード
+  assign load2 = !op[2] & op[3]; // 出力ポートへロード
+  assign load3 = (!co_reg | op[0]) & op[2] & op[3]; // PCへロード
 
 
-wire[1:0]select;
-assign select = ui_in[1:0]; 
-reg [11:0] contador;
-reg [7:0] q;
-    
-// Assign the input.
-assign uio_out = 8'h00;
-assign uio_oe = 8'h00;
+endmodule
 
-always @ (posedge rst_n or posedge clk)
-if (rst_n)
-contador<=12'b000000000000;
-else if(select==2'b00 || select==2'b11) begin
-if (contador <63)
-contador <= contador + 1;
-else
-contador <= 0;
-end
-else if(select==2'b01 || select==2'b10) begin
-if (contador <76)
-contador <= contador + 1;
-else
-contador <= 0;
-end
-always @ (posedge clk)
-if(select==2'b00 || select==2'b11)
-begin
-if(contador == 'd0)
-begin
-q<=8'b10100001;
-end
-else if(contador == 'd1)
-begin
-q<=8'b01010011;
-end
-else if(contador == 'd2)
-begin
-q<=8'b01101111;
-end
-else if(contador == 'd3)
-begin
-q<=8'b01111001;
-end
-else if(contador == 'd4)
-begin
-q<=8'b00100000;
-end
-else if(contador == 'd5)
-begin
-q<=8'b01100100;
-end
-else if(contador == 'd6)
-begin
-q<=8'b01100101;
-end
-else if(contador == 'd7)
-begin
-q<=8'b00100000;
-end
-else if(contador == 'd8)
-begin
-q<=8'b01011010;
-end
-else if(contador == 'd9)
-begin
-q<=8'b01100001;
-end
-else if(contador == 'd10)
-begin
-q<=8'b01100011;
-end
-else if(contador == 'd11)
-begin
-q<=8'b01100001;
-end
-else if(contador == 'd12)
-begin
-q<=8'b01110000;
-end
-else if(contador == 'd13)
-begin
-q<=8'b01100001;
-end
-else if(contador == 'd14)
-begin
-q<=8'b00100001;
-end
-else if(contador == 'd15)
-begin
-q<=8'b00100000;
-end
-else if(contador == 'd16)
-begin
-q<=8'b01110100;
-end
-else if(contador == 'd17)
-begin
-q<=8'b01101001;
-end
-else if(contador == 'd18)
-begin
-q<=8'b01100101;
-end
-else if(contador == 'd19)
-begin
-q<=8'b01110010;
-end
-else if(contador == 'd20)
-begin
-q<=8'b01110010;
-end
-else if(contador == 'd21)
-begin
-q<=8'b01100001;
-end
-else if(contador == 'd22)
-begin
-q<=8'b00100000;
-end
-else if(contador == 'd23)
-begin
-q<=8'b01100011;
-end
-else if(contador == 'd24)
-begin
-q<=8'b01100001;
-end
-else if(contador == 'd25)
-begin
-q<=8'b01101100;
-end
-else if(contador == 'd26)
-begin
-q<=8'b01101001;
-end
-else if(contador == 'd27)
-begin
-q<=8'b01100101;
-end
-else if(contador == 'd28)
-begin
-q<=8'b01101110;
-end
-else if(contador == 'd29)
-begin
-q<=8'b01110100;
-end
-else if(contador == 'd30)
-begin
-q<=8'b01100101;
-end
-else if(contador == 'd31)
-begin
-q<=8'b00101100;
-end
-else if(contador == 'd32)
-begin
-q<=8'b00001010;
-end
-else if(contador == 'd33)
-begin
-q<=8'b01101110;
-end
-else if(contador == 'd34)
-begin
-q<=8'b01100001;
-end
-else if(contador == 'd35)
-begin
-q<=8'b01100011;
-end
-else if(contador == 'd36)
-begin
-q<=8'b11101101;
-end
-else if(contador == 'd37)
-begin
-q<=8'b00100000;
-end
-else if(contador == 'd38)
-begin
-q<=8'b01100101;
-end
-else if(contador == 'd39)
-begin
-q<=8'b01101110;
-end
-else if(contador == 'd40)
-begin
-q<=8'b00100000;
-end
-else if(contador == 'd41)
-begin
-q<=8'b01100101;
-end
-else if(contador == 'd42)
-begin
-q<=8'b01101100;
-end
-else if(contador == 'd43)
-begin
-q<=8'b00100000;
-end
-else if(contador == 'd44)
-begin
-q<=8'b01100010;
-end
-else if(contador == 'd45)
-begin
-q<=8'b01100001;
-end
-else if(contador == 'd46)
-begin
-q<=8'b01110010;
-end
-else if(contador == 'd47)
-begin
-q<=8'b01110010;
-end
-else if(contador == 'd48)
-begin
-q<=8'b01101001;
-end
-else if(contador == 'd49)
-begin
-q<=8'b01101111;
-end
-else if(contador == 'd50)
-begin
-q<=8'b00100000;
-end
-else if(contador == 'd51)
-begin
-q<=8'b01000101;
-end
-else if(contador == 'd52)
-begin
-q<=8'b01101100;
-end
-else if(contador == 'd53)
-begin
-q<=8'b00100000;
-end
-else if(contador == 'd54)
-begin
-q<=8'b01010100;
-end
-else if(contador == 'd55)
-begin
-q<=8'b01100001;
-end
-else if(contador == 'd56)
-begin
-q<=8'b01101101;
-end
-else if(contador == 'd57)
-begin
-q<=8'b01100001;
-end
-else if(contador == 'd58)
-begin
-q<=8'b01110010;
-end
-else if(contador == 'd59)
-begin
-q<=8'b01101001;
-end
-else if(contador == 'd60)
-begin
-q<=8'b01101110;
-end
-else if(contador == 'd61)
-begin
-q<=8'b01100100;
-end
-else if(contador == 'd62)
-begin
-q<=8'b01100001;
-end
-else if(contador == 'd63)
-begin
-q<=8'b01101100;
-end
-end
-else if(select==2'b01 || select==2'b10)
-begin
-if(contador == 'd0)
-begin
-q<=8'b01010000;
-end
-else if(contador == 'd1)
-begin
-q<=8'b01010000;
-end
-else if(contador == 'd2)
-begin
-q<=8'b01110010;
-end
-else if(contador == 'd3)
-begin
-q<=8'b01101111;
-end
-else if(contador == 'd4)
-begin
-q<=8'b01111001;
-end
-else if(contador == 'd5)
-begin
-q<=8'b01100101;
-end
-else if(contador == 'd6)
-begin
-q<=8'b01100011;
-end
-else if(contador == 'd7)
-begin
-q<=8'b01110100;
-end
-else if(contador == 'd8)
-begin
-q<=8'b01101111;
-end
-else if(contador == 'd9)
-begin
-q<=8'b00100000;
-end
-else if(contador == 'd10)
-begin
-q<=8'b01110100;
-end
-else if(contador == 'd11)
-begin
-q<=8'b01110010;
-end
-else if(contador == 'd12)
-begin
-q<=8'b01100001;
-end
-else if(contador == 'd13)
-begin
-q<=8'b01100010;
-end
-else if(contador == 'd14)
-begin
-q<=8'b01100001;
-end
-else if(contador == 'd15)
-begin
-q<=8'b01101010;
-end
-else if(contador == 'd16)
-begin
-q<=8'b01100001;
-end
-else if(contador == 'd17)
-begin
-q<=8'b01100100;
-end
-else if(contador == 'd18)
-begin
-q<=8'b01101111;
-end
-else if(contador == 'd19)
-begin
-q<=8'b00100000;
-end
-else if(contador == 'd20)
-begin
-q<=8'b01110000;
-end
-else if(contador == 'd21)
-begin
-q<=8'b01101111;
-end
-else if(contador == 'd22)
-begin
-q<=8'b01110010;
-end
-else if(contador == 'd23)
-begin
-q<=8'b00100000;
-end
-else if(contador == 'd24)
-begin
-q<=8'b01000001;
-end
-else if(contador == 'd25)
-begin
-q<=8'b01101110;
-end
-else if(contador == 'd26)
-begin
-q<=8'b01100111;
-end
-else if(contador == 'd27)
-begin
-q<=8'b01100101;
-end
-else if(contador == 'd28)
-begin
-q<=8'b01101100;
-end
-else if(contador == 'd29)
-begin
-q<=8'b00100000;
-end
-else if(contador == 'd30)
-begin
-q<=8'b01001111;
-end
-else if(contador == 'd31)
-begin
-q<=8'b01110010;
-end
-else if(contador == 'd32)
-begin
-q<=8'b01100101;
-end
-else if(contador == 'd33)
-begin
-q<=8'b01101100;
-end
-else if(contador == 'd34)
-begin
-q<=8'b01101100;
-end
-else if(contador == 'd35)
-begin
-q<=8'b01100001;
-end
-else if(contador == 'd36)
-begin
-q<=8'b01101110;
-end
-else if(contador == 'd37)
-begin
-q<=8'b01100001;
-end
-else if(contador == 'd38)
-begin
-q<=8'b00101100;
-end
-else if(contador == 'd39)
-begin
-q<=8'b00100000;
-end
-else if(contador == 'd40)
-begin
-q<=8'b01000100;
-end
-else if(contador == 'd41)
-begin
-q<=8'b01100001;
-end
-else if(contador == 'd42)
-begin
-q<=8'b01101110;
-end
-else if(contador == 'd43)
-begin
-q<=8'b01101001;
-end
-else if(contador == 'd44)
-begin
-q<=8'b01100101;
-end
-else if(contador == 'd45)
-begin
-q<=8'b01101100;
-end
-else if(contador == 'd46)
-begin
-q<=8'b00100000;
-end
-else if(contador == 'd47)
-begin
-q<=8'b01001101;
-end
-else if(contador == 'd48)
-begin
-q<=8'b01110101;
-end
-else if(contador == 'd49)
-begin
-q<=8'b01101110;
-end
-else if(contador == 'd50)
-begin
-q<=8'b01100100;
-end
-else if(contador == 'd51)
-begin
-q<=8'b01101111;
-end
-else if(contador == 'd52)
-begin
-q<=8'b00101100;
-end
-else if(contador == 'd53)
-begin
-q<=8'b00100000;
-end
-else if(contador == 'd54)
-begin
-q<=8'b01001010;
-end
-else if(contador == 'd55)
-begin
-q<=8'b01110101;
-end
-else if(contador == 'd56)
-begin
-q<=8'b01101100;
-end
-else if(contador == 'd57)
-begin
-q<=8'b01101001;
-end
-else if(contador == 'd58)
-begin
-q<=8'b01101111;
-end
-else if(contador == 'd59)
-begin
-q<=8'b00100000;
-end
-else if(contador == 'd60)
-begin
-q<=8'b01001100;
-end
-else if(contador == 'd61)
-begin
-q<=8'b01101111;
-end
-else if(contador == 'd62)
-begin
-q<=8'b01110000;
-end
-else if(contador == 'd63)
-begin
-q<=8'b01100101;
-end
-else if(contador == 'd64)
-begin
-q<=8'b01111010;
-end
-else if(contador == 'd65)
-begin
-q<=8'b00100000;
-end
-else if(contador == 'd66)
-begin
-q<=8'b01111001;
-end
-else if(contador == 'd67)
-begin
-q<=8'b00100000;
-end
-else if(contador == 'd68)
-begin
-q<=8'b01001110;
-end
-else if(contador == 'd69)
-begin
-q<=8'b01101111;
-end
-else if(contador == 'd70)
-begin
-q<=8'b01100101;
-end
-else if(contador == 'd71)
-begin
-q<=8'b01101100;
-end
-else if(contador == 'd72)
-begin
-q<=8'b00100000;
-end
-else if(contador == 'd73)
-begin
-q<=8'b01010000;
-end
-else if(contador == 'd74)
-begin
-q<=8'b01110010;
-end
-else if(contador == 'd75)
-begin
-q<=8'b01100001;
-end
-else if(contador == 'd76)
-begin
-q<=8'b01100100;
-end
-end
-assign uo_out = q;
+//------------------------
+// データセレクタ
+//
+// select_a、select_bの信号に応じてc0〜c3の信号を出力する
+//
+// * (select_a = L, select_b = L) => c0を出力
+// * (select_a = H, select_b = L) => c1を出力
+// * (select_a = L, select_b = H) => c2を出力
+// * (select_a = H, select_b = H) => c3を出力
+//------------------------
+module data_selector(
+  input [3:0] c0,
+  input [3:0] c1,
+  input [3:0] c2,
+  input [3:0] c3,
+  input select_a, select_b,
+  output reg [3:0] y
+);
+
+  always @(*) begin
+    if (select_a & select_b)
+      y = c3;
+    else if (!select_a & select_b)
+      y = c2;
+    else if (select_a & !select_b)
+      y = c1;
+    else
+      y = c0;
+  end
+endmodule
+
+
+//------------------------
+// テストベンチ
+// （ラーメンタイマーを実行して、out を $display するだけ）
+// iverilog -o a cpu.v && ./a
+//------------------------
+module cpu_test();
+  reg clk;
+  reg n_reset;
+  reg [3:0] port_in;
+
+  wire [3:0] address;
+  wire [7:0] dout;
+  wire [3:0] port_out;
+
+  // Generate clock
+  always begin
+    #5 clk = 1;
+    #5 clk = 0;
+  end
+
+  cpu cpu(clk, n_reset, address, dout, port_in, port_out);
+  test_rom rom(address, dout);
+
+  // Finish after 3000 unit times
+  always
+    #3000 $finish;
+
+  initial begin
+    // 波形データを cpu_test.vcd へ出力する
+    $dumpfile("cpu_test.vcd");
+
+    // cpu モジュール内の変数を波形データとして出力
+    $dumpvars(0, cpu);
+
+    // 出力ポートをモニタする
+    $monitor("%t: out = %b", $time, port_out);
+  end
+
+  initial begin
+    // Init variables
+    #0 clk = 0; n_reset = 1; port_in = 4'b0101;
+
+    // Reset cpu
+    #10 n_reset = 0;
+    #10 n_reset = 1;
+  end
+endmodule
+
+//------------------------
+// ROM
+//------------------------
+module test_rom(
+  input [3:0] address,
+  output reg [7:0] dout
+);
+
+  always @(address)
+    case (address)
+      /*
+        ラーメンタイマー
+      */
+      4'b0000: dout <= 8'b10110111;
+      4'b0001: dout <= 8'b00000001;
+      4'b0010: dout <= 8'b11100001;
+      4'b0011: dout <= 8'b00000001;
+      4'b0100: dout <= 8'b11100011;
+      4'b0101: dout <= 8'b10110110;
+      4'b0110: dout <= 8'b00000001;
+      4'b0111: dout <= 8'b11100110;
+      4'b1000: dout <= 8'b00000001;
+      4'b1001: dout <= 8'b11101000;
+      4'b1010: dout <= 8'b10110000;
+      4'b1011: dout <= 8'b10110100;
+      4'b1100: dout <= 8'b00000001;
+      4'b1101: dout <= 8'b11101010;
+      4'b1110: dout <= 8'b10111000;
+      4'b1111: dout <= 8'b11111111;
+      default: dout <= 8'bxxxxxxxx;
+    endcase
 endmodule
